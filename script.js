8aaadd83a09bc230072ffcdcb9475a1b8a96ef2b
SeviNy0 <sevinyo0909@gmail.com>
2025-11-09 11:44:19 -0500

    Correcciones 3

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const SERVICE_UUID = '0000ffe1-0000-1000-8000-00805f9b34fb';
-const CHARACTERISTIC_UUID = '0000ffe3-0000-1000-8000-00805f9b34fb';
+const SERVICE_UUID = '0000ffe0-0000-1000-8000-00805f9b34fb'; 
+const CHARACTERISTIC_UUID = '0000ffe1-0000-1000-8000-00805f9b34fb'; 
 const PACKET_ON = new Uint8Array([0xAA, 0x55, 0x00, 0x00, 0x00, 0x01, 0x64, 0x1E, 0x01, 0x99]);
 const PACKET_OFF = new Uint8Array([0xAA, 0x55, 0x00, 0x00, 0x00, 0x00, 0x64, 0x1E, 0x01, 0x99]);
 const DEVICE_NAME_PREFIX = 'CP017'; 
-
 async function controlLuz(action) {
     document.getElementById('log').innerText = `Estado: Conectando para ${action}...`; 
     const packet = (action === 'ON') ? PACKET_ON : PACKET_OFF;
@@ -18,8 +17,9 @@ async function controlLuz(action) {
         
         const server = await device.gatt.connect();
         document.getElementById('log').innerText = `Estado: Conectado. Enviando ${action}...`;
-        const service = await server.getPrimaryService(SERVICE_UUID);
+        const service = await server.getPrimaryService(SERVICE_UUID); 
         const characteristic = await service.getCharacteristic(CHARACTERISTIC_UUID); 
+        
         await characteristic.writeValue(packet);
         document.getElementById('log').innerText = `✅ Comando ${action} enviado con éxito.`;
         server.disconnect(); 
