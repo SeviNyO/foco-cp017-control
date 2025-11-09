821e07c8bdc4544da2a661700ca1fd92cd1b6d11
SeviNy0 <sevinyo0909@gmail.com>
2025-11-09 11:58:26 -0500

    Correcciones 6

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Constantes de Control (Servicios que USAMOS)
-const SERVICE_UUID = '0000ffe0-0000-1000-8000-00805f9b34fb'; // FFE0 (Servicio de Control)
-const CHARACTERISTIC_UUID = '0000ffe1-0000-1000-8000-00805f9b34fb'; // FFE1 (Característica de Escribir)
+const SERVICE_UUID = '0000ffe0-0000-1000-8000-00805f9b34fb'; // FFE0 (Correcto)
+const CHARACTERISTIC_UUID = '0000ffe1-0000-1000-8000-00805f9b34fb'; // FFE1 (Correcto)
 
-// Constante de Anuncio (Servicio que BUSCAMOS)
-const ADVERTISING_UUID = '0000af30-0000-1000-8000-00805f9b34fb'; // AF30 (El que anuncia)
+// Constante de Nombre (El Filtro que BUSCAMOS)
+const DEVICE_NAME_PREFIX = 'CP017'; // (Correcto)
 
-// Paquetes y Nombre (Esto está bien)
+// Paquetes (Esto está bien)
 const PACKET_ON = new Uint8Array([0xAA, 0x55, 0x00, 0x00, 0x00, 0x01, 0x64, 0x1E, 0x01, 0x99]);
 const PACKET_OFF = new Uint8Array([0xAA, 0x55, 0x00, 0x00, 0x00, 0x00, 0x64, 0x1E, 0x01, 0x99]);
 
@@ -16,11 +16,11 @@ async function controlLuz(action) {
     try{
         document.getElementById('log').innerText = `¡Permiso! Busca y selecciona 'CP017' en la lista...`;
 
-        // --- CAMBIO CLAVE (El Filtro Correcto) ---
+        // --- CAMBIO CLAVE (Volvemos al Filtro de Nombre) ---
         const device = await navigator.bluetooth.requestDevice(
             {
-                // Filtramos por el servicio que SÍ se anuncia (AF30)
-                filters: [{ services: [ADVERTISING_UUID] }],
+                // Filtramos por el nombre que SÍ vemos
+                filters: [{ namePrefix: DEVICE_NAME_PREFIX }],
                 
                 // Pedimos permiso para USAR el servicio FFE0 (el de control)
                 optionalServices: [SERVICE_UUID] 
